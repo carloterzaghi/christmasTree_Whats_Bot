b6136d459446593073fa3217d342b08f68f25420
carloterzaghi <carlotts16@gmail.com>
2022-12-15 15:23:57 -0300

    Commit 1

---
+++ commands/comando_musica.py
@@ -0,0 +1,13 @@
+def comando_musica(enviado,lista_playlist):
+    if enviado == 'som':
+        return
+    elif enviado == 'playlist':
+        musicas = ''
+        for i in lista_playlist: musicas += f'\n- {i}'
+        return f'Playlist: {musicas}'
+    elif 'add' in enviado:
+        lista_playlist.append(int(enviado.replace('add', '')))
+        return f"Música {str(int(enviado.replace('add', '')))} adicionada."
+    elif enviado == 'skip':
+        return
+    
---
+++ commands/comando_placa.py
@@ -0,0 +1,22 @@
+from twilio.twiml.messaging_response import MessagingResponse
+import serial
+
+def comando_ligar_Placa(enviado):
+    try:
+        serialInst = serial.Serial()
+        serialInst.baudrate = 9600
+        serialInst.port = 'COM4'
+        serialInst.open()
+
+        if enviado in ['ligar','desligar']:
+            serialInst.write(('O' if enviado == 'ligar' else 'L').encode('utf-8'))
+            p = serialInst.readline()
+            return [f'LED {enviado.capitalize()[0:-1]}do.',True]
+        else:
+            return ['',False]
+    #Não conseguiu conectar com a placa
+    except:
+        if enviado in ['ligar','desligar']:
+            return ['Placa Desconectada',True]
+        else:
+            return ['',False]
