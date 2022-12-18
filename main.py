c8be621efa64ea783258e31dd78f221b528fcac0
carloterzaghi <carlotts16@gmail.com>
2022-12-18 12:34:33 -0300

    Arrumei o DB

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from commands.comando_placa import comando_ligar_Placa
 from commands.comando_musica import comando_musica
-import firebase_admin
-from firebase_admin import credentials
-
-global lista_playlist
-lista_playlist = []
 
 app = Flask(__name__)
   
@@ -16,9 +11,7 @@ def index():
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
-    cred = credentials.Certificate("serviceAccountKey.json")
-    firebase_admin.initialize_app(cred)
-    #Lista Playlist + Lista de comandos + Teste da Placa
+    #Lista de comandos + Teste da Placa
     lista_comandos = ['ligar','desligar','som', 'playlist', 'add','skip']
     placa_teste = False
 
@@ -45,13 +38,7 @@ def sms_reply():
     
     #Comandos de Som
     elif enviado.split()[0] in lista_comandos:
-        print(lista_playlist)
-        if type(comando_musica(enviado,lista_playlist)) == list:
-            lista_playlist = comando_musica(enviado,lista_playlist)[1]
-            print(lista_playlist)
-            msg.body(comando_musica(enviado,lista_playlist)[0])
-        else:
-            msg.body(comando_musica(enviado,lista_playlist))
+        msg.body(comando_musica(enviado))
     else:
         msg.body(f'Não entendi o que você disse.')
 
