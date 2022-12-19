3172e88888e79d55c755df251733dc6547b282fc
carloterzaghi <carlotts16@gmail.com>
2022-12-18 21:44:30 -0300

    Adicionado o DB Firebase e já pré config

--- commands/comando_musica.py
+++ commands/comando_musica.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
 
 def comando_musica(enviado):
     #Init DB Firebase + Lista de comandos + Teste da Placa
@@ -17,18 +18,26 @@ def comando_musica(enviado):
     elif enviado == 'playlist':
         musicas = ''
         for doc in db.collection('Playlist').get(): 
-            for i in sorted(doc.to_dict()): musicas += f'\n➤ *{doc.to_dict()[i]}*'
+            for i in sorted(doc.to_dict()): 
+                for fileName in os.listdir(r'./Musicas'):
+                    if fileName[0:2] == ('0'+str(int(doc.to_dict()[i])) if len(str(int(doc.to_dict()[i]))) == 1 else str(int(doc.to_dict()[i]))): 
+                        musicas += f'\n➤ *{fileName[5:-4]}*'
         return f'Playlist: {musicas}'
     # Retorna e Adiciona o numero da musica
     elif 'add' in enviado:
-        if 0 < int(enviado.replace('add', '')) <= 25:
-            numero = 1
-            for doc in db.collection('Playlist').get(): 
-                for i in doc.to_dict(): numero+=1
-            db.collection('Playlist').document('list').update({str(numero):enviado.replace('add', '')})
-            return f"Música {str(int(enviado.replace('add', '')))} adicionada."
-        else:
-            return f"Música {str(int(enviado.replace('add', '')))} não existe."
+        try:
+            if 0 < int(enviado.replace('add', '')) <= 25:
+                numero = 1
+                for doc in db.collection('Playlist').get(): 
+                    for i in doc.to_dict(): numero+=1
+                db.collection('Playlist').document('list').update({str(numero):enviado.replace('add', '')})
+                for fileName in os.listdir(r'./Musicas'):
+                    if fileName[0:2] == ('0'+str(int(enviado.replace('add', ''))) if len(str(int(enviado.replace('add', '')))) == 1 else str(int(enviado.replace('add', '')))): 
+                        return f"Música *{fileName[5:-4]}* adicionada."
+            else:
+                return f"Música *{str(int(enviado.replace('add', '')))}* não existe."
+        except:
+            return "Erro no comando."
     # Pula a musica
     elif enviado == 'skip':
         return
